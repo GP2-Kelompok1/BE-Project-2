d102fc8e4bef6175035cf220a5a34e49f05317fa
febryanmz <febryan.zainal@gmail.com>
2022-12-06 17:15:55 +0700

    gcp/fix3

--- features/user/delivery/request.go
+++ features/user/delivery/request.go
@@ -5,21 +5,23 @@ import (
 )
 
 type UserRequest struct {
-	Full_Name  string        `json:"full_name" form:"full_name"`
-	Email      string        `json:"email" form:"email"`
-	Password   string        `json:"password" form:"password"`
-	Teams      user.CoreTeam `json:"team_id" form:"team_id"`
-	Role       string        `json:"role" form:"role"`
-	Status     string        `json:"status" form:"status"`
-	Permission string        `json:"permission" form:"permission"`
+	Full_Name  string `json:"full_name" form:"full_name"`
+	Email      string `json:"email" form:"email"`
+	Password   string `json:"password" form:"password"`
+	TeamID     uint   `json:"team_id" form:"team_id"`
+	Role       string `json:"role" form:"role"`
+	Status     string `json:"status" form:"status"`
+	Permission string `json:"permission" form:"permission"`
 }
 
 func toCore(data UserRequest) user.CoreUser {
 	return user.CoreUser{
-		Full_Name:  data.Full_Name,
-		Email:      data.Email,
-		Password:   data.Password,
-		Teams:      data.Teams,
+		Full_Name: data.Full_Name,
+		Email:     data.Email,
+		Password:  data.Password,
+		Teams: user.CoreTeam{ // <<< lihat ini
+			ID: data.TeamID,
+		},
 		Role:       data.Role,
 		Status:     data.Status,
 		Permission: data.Permission,
